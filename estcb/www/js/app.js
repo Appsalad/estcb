83af9c11174f9e7a1e4abf648e77d2443a756f42
Pedro Serra <pedro.rserra@gmail.com>
2013-04-10 16:22:50 +0100

    link android + ios code

--- estcb/www/js/app.js
+++ estcb/www/js/app.js
@@ -1,9 +1,39 @@
 (function(app, undefined) {
-    app.initialize = function() {
-        document.addEventListener('deviceready', app.ready, false);
-    };
 
-    app.ready = function() {
+    _.extend(app, {
+        //
+        // App domain structure
+        data: {},
+        models: {},
+        collections: {},
+        views: {
+            screens: {}
+        },
+        //
+        // Initialization of structures
+        initialize: function() {
+            // retrieve data
+            app.data.news = new app.collections.News();
+            app.data.news.fetch();
+            app.data.jobs = new app.collections.Jobs();
+            app.data.jobs.fetch();
+            app.data.warnings = new app.collections.Warnings();
+            app.data.warnings.fetch();
 
-    };
-})(app = window.app || {});
+            if (!navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
+                app.ready(); //this is the browser
+            }
+        },
+        //
+        // Your device is ready!
+        ready: function() {
+            // Add header + menu
+            new app.views.Header().render().$el.appendTo('body');
+            new app.views.Menu().render().$el.appendTo('body');
+
+            // Open first screen
+            app.screen.open({screen: new app.views.screens.News()});
+        }
+    });
+
+})(window.app = window.app || {});
